cdb85872cc683f8bbff91027e7fecb00e18d2bdc
Nathan Grasset <ngrasset@student.42.fr>
2018-01-17 16:59:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ module.exports = (iterable, promisifier, initialValue = []) =>
   iterable.reduce(
     (chain, elem, index) =>
       chain.then(chainRes =>
-        promisifier(elem, index, iterable, chainRes).then(
-          res => (chainRes.push(res), chainRes)
-        )
+        promisifier(elem, index, iterable, chainRes)
+          .then(res => chainRes.concat(res))
       ),
     Promise.resolve(initialValue)
   );
 
 // Alternative implementation with an array of function
 // Not exported yet, I'm just leaving it there to think about it
-const alterPromiseEach = (iterable, initialValue) =>
+const alterPromiseEach = (iterable, initialValue = []) =>
   iterable.reduce(
-    (chain, elem, index) => chain.then(res => elem(res, index)),
+    (chain, elem, index) =>
+      chain.then(chainRes => elem(res, index))
+        .then(res => chainRes.concat(res)),
     Promise.resolve(initialValue)
   );
